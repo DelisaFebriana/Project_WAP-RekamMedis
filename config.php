952e0960a5e75e1aa776cb8dafd15d87b007978e
DelisaFebriana <147138967+DelisaFebriana@users.noreply.github.com>
2024-01-01 06:45:32 +0700

    coomit pertama

---
+++ config.php
@@ -0,0 +1,60 @@
+<?php 
+
+date_default_timezone_set('Asia/Jakarta');
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'db_rekammedis';
+
+$koneksi = mysqli_connect($host, $user, $pass, $dbname);
+
+// if(!$koneksi){
+//     die('Koneksi gagal');
+// } else {
+//     echo "Koneksi berhasil";
+// }
+
+$main_url = "http://localhost/rekammedis-app/";
+
+function uploadGbr($url){
+    $namafile = $_FILES['gambar']['name'];
+    $ukuran   = $_FILES['gambar']['size'];
+    $tmp      = $_FILES['gambar']['tmp_name'];
+
+    $ekstensiValid = ['jpg','jpeg','png','gif'];
+    $ekstensiFile  = explode('.', $namafile);
+    $ekstensiFile  = strtolower(end($ekstensiFile));
+
+    if (!in_array($ekstensiFile, $ekstensiValid)) {
+        echo "<script>
+                alert('Input user gagal, file yang anda apload bukan gambar');
+                 window.location = '$url';
+        </script>"; 
+        die();
+    }
+
+    if ($ukuran > 1000000) {
+        echo "<script>
+                alert('Input user gagal, maximal ukuran gambar 1 MB');
+                window.location = '$url';
+         </script>"; 
+         die(); 
+    }
+
+    $namafileBaru = time() . '-' . $namafile;
+
+    move_uploaded_file($tmp, '../asset/gambar/' . $namafileBaru);
+    return $namafileBaru;
+
+}
+
+// function in_date($tgl){
+//     $dd = substr($tgl, 8, 2);
+//     $mm = substr($tgl, 5, 2);
+//     $yy = substr($tgl, 0, 4);
+
+//     return $dd . "-" . $mm . "-" . $yy;
+// }
+
+?>
---
+++ template/footer.php
@@ -0,0 +1,17 @@
+</div>
+</div>
+<script src="<?= $main_url ?>asset/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" 
+    integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" 
+    crossorigin="anonymous"></script>
+    <script src="<?= $main_url ?>asset/dashboard/dashboard.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+
+    <script>
+        let table = new DataTable('#myTable')
+    </script>
+
+</body>
+</html>
